556068a7cffe147743c05ac539c45b2121bd2b03
filipposanfilippo <filipposanfilippo@caf7e6e2-c23a-11de-a250-c7f789e21d83>
2009-10-28 17:44:51 +0000

    modified files: ClientMobileInterface ClientMobile
    @Erik: have a look at the code and give me a feedback.
    ps: bring some Italian fellas!

--- SocialLife/src/ClientMobileInterface.java
+++ SocialLife/src/ClientMobileInterface.java
@@ -4,13 +4,13 @@ import java.rmi.RemoteException;
 
 public interface ClientMobileInterface extends Remote{
 	public String getPsw() throws RemoteException;		//?
-	public boolean register(String username, String password, String sex, String age) throws RemoteException;
-	public String eventsList(String criterion) throws RemoteException;
-	public boolean joinEvent(String eventCode)throws RemoteException;
-	public boolean inviteFriend(String friendPhone)throws RemoteException;
-	public boolean myLocation(String myLocation)throws RemoteException;
-	public String userList(String criterion)throws RemoteException;
-	public boolean broadcastMyStatus	(String criterion)throws RemoteException;
-	public boolean chatUp(String nickname)throws RemoteException;
+	public boolean register(String senderPhone, String username, String password, String sex, String age) throws RemoteException;
+	public String eventsList(String senderPhone, String criterion) throws RemoteException;
+	public boolean joinEvent(String senderPhone, String eventCode)throws RemoteException;
+	public boolean inviteFriend(String senderPhone, String friendPhone)throws RemoteException;
+	public boolean myLocation(String senderPhone, String myLocation)throws RemoteException;
+	public String userList(String senderPhone, String criterion)throws RemoteException;
+	public boolean broadcastMyStatus	(String senderPhone, String criterion)throws RemoteException;
+	public boolean chatUp(String senderPhone, String nickname)throws RemoteException;
 	public boolean checkRegistration(String phoneNumber)throws RemoteException;
 }
