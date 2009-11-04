d114aa4cbc2a8ffe79c90de477e832699c4e9311
filipposanfilippo <filipposanfilippo@caf7e6e2-c23a-11de-a250-c7f789e21d83>
2009-11-04 16:32:39 +0000

    added mobile method to interface

--- Merge/src/ServerInterface.java
+++ Merge/src/ServerInterface.java
@@ -30,4 +30,32 @@ public interface ServerInterface extends Remote {
 
 	public MyEvent createEvent(String nameEvent, String description)
 			throws RemoteException;
+
+	// __________________________
+	public boolean register(String senderPhone, String username,
+			String password, String sex, String age) throws RemoteException;
+
+	public String eventsList(String senderPhone, String criterion)
+			throws RemoteException;
+
+	public boolean joinEvent(String senderPhone, String eventCode)
+			throws RemoteException;
+
+	public boolean inviteFriend(String senderPhone, String friendPhone)
+			throws RemoteException;
+
+	public boolean myLocation(String senderPhone, String myLocation)
+			throws RemoteException;
+
+	public String userList(String senderPhone, String criterion)
+			throws RemoteException;
+
+	public boolean broadcastMyStatus(String senderPhone, String criterion)
+			throws RemoteException;
+
+	public boolean chatUp(String senderPhone, String nickname)
+			throws RemoteException;
+
+	public boolean checkRegistration(String phoneNumber) throws RemoteException;
+	// __________________________
 }
