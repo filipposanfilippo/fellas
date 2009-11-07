30adc4de7aa5bc9ab261f8769a1c1e6d2d567ca5
lorenzo.sciuto <lorenzo.sciuto@caf7e6e2-c23a-11de-a250-c7f789e21d83>
2009-11-07 15:16:24 +0000

    New repository, first commit.

---
+++ Merge/src/fellas/ClubUser.java
@@ -0,0 +1,16 @@
+package fellas;
+
+import java.util.ArrayList;
+
+// server class
+public class ClubUser {
+	String oName;
+	String oSurname;
+	String cAddress;
+	String cTel;
+	String cName;
+	String psw;
+
+	ArrayList<MyEvent> eventList = new ArrayList<MyEvent>();
+
+}
